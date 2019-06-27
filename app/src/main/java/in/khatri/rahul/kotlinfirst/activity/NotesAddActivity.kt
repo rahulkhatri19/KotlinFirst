1423fc5584d3dbc2470012ddf9f3e0aa542f2831
Rahul Khatri <khatri.rahul019@gmail.com>
2019-06-27 23:17:42 +0530

    WIP Sqlite

--- app/src/main/java/in/khatri/rahul/kotlinfirst/activity/NotesAddActivity.kt
+++ app/src/main/java/in/khatri/rahul/kotlinfirst/activity/NotesAddActivity.kt
@@ -1,6 +1,8 @@
 package `in`.khatri.rahul.kotlinfirst.activity
 
 import `in`.khatri.rahul.kotlinfirst.R
+import `in`.khatri.rahul.kotlinfirst.utility.DbManager
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -14,12 +16,15 @@ class NotesAddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes_add)
 
+
         btn_add.setOnClickListener {
-            Toast.makeText(this, "Title: "+et_title.text+"\nDescription: "+et_des.text,Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Title: " + et_title.text + "\nDescription: " + et_des.text, Toast.LENGTH_LONG).show()
+
         }
 //        et_title.text
 //        et_des.text
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.notes_menu, menu)
@@ -27,9 +32,24 @@ class NotesAddActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when(item!!.itemId) {
-            R.id.add_note -> Toast.makeText(this, "Title: "+et_title.text+"\nDescription: "+et_des.text,Toast.LENGTH_LONG).show()
+        when (item!!.itemId) {
+            R.id.add_note -> {
+                Toast.makeText(this, "Title: " + et_title.text + "\nDescription: " + et_des.text, Toast.LENGTH_LONG)
+                    .show()
+                addNotes(et_title.text.toString(), et_des.text.toString())
+            }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun addNotes(title: String, des: String) {
+        val dbManager = DbManager(this)
+        var values = ContentValues()
+        values.put("Title", title)
+        values.put("Description", des)
+        val ID = dbManager.Insert(values)
+        if (ID > 0) Toast.makeText(this, "Note is been Inserted", Toast.LENGTH_SHORT).show()
+        else Toast.makeText(this, "Cant't add note", Toast.LENGTH_SHORT).show()
+
     }
 }
--- app/src/main/java/in/khatri/rahul/kotlinfirst/utility/DbManager.kt
+++ app/src/main/java/in/khatri/rahul/kotlinfirst/utility/DbManager.kt
@@ -1,12 +1,46 @@
 package `in`.khatri.rahul.kotlinfirst.utility
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
 class DbManager {
 
-    val dbName="MyNotes"
-    val dbTableName="Notes"
-    val colID="ID"
-    val colTitle= "Title"
-    val colDes= "Description"
-    val dbVersion="1"
-    val sqlCreateTable= "CREATE TABLE IF NOT EXISTS $dbName "
+    val dbName = "MyNotes"
+    val dbTableName = "Notes"
+    val colID = "ID"
+    val colTitle = "Title"
+    val colDes = "Description"
+    val dbVersion = 1
+    val sqlCreateTable = "CREATE TABLE IF NOT EXISTS $dbName ($colID INTEGER PRIMARY KEY,$colTitle TEXT, $colDes TEXT);"
+    var sqlDB: SQLiteDatabase? = null
+
+    constructor(context: Context) {
+        val db = DatabaseHelperNotes(context)
+        sqlDB = db.writableDatabase
+    }
+
+    inner class DatabaseHelperNotes : SQLiteOpenHelper {
+        var context: Context? = null
+
+        constructor(context: Context) : super(context, dbName, null, dbVersion) {
+            this.context = context
+        }
+
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL(sqlCreateTable)
+            Toast.makeText(this.context, "Database created", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            db!!.execSQL("Drop table IF EXISTS $dbTableName")
+        }
+    }
+
+    fun Insert(values: ContentValues): Long {
+        val ID = sqlDB!!.insert(dbTableName, "", values)
+        return ID
+    }
 }
